5e9b978a8a9e1a71efc0c604195ffa105afdd29c
Kyren Lee <the.kyren@gmail.com>
2023-02-16 23:14:12 -0500

    added subdir

---
+++ madlib_1.py
@@ -0,0 +1,28 @@
+#extracting initial paragraph from txt file
+with open('madlib_sourceparagraph.txt', 'r') as sourceparagraph:
+    paragraph= sourceparagraph.read()
+
+#exclusion words
+exclusionwords= ['where', 'when', 'what', 'from', 'could', '''couldn't''', 'would', '''wouldn't''', 'place']
+
+#turning the paragraph into a list of words so that each word has an index
+paragraph_list= paragraph.split(' ')
+
+#choosing the words to blank out from the list
+mad_lib_list=[]
+for i in range(len(paragraph_list)):
+    if paragraph_list[i] not in exclusionwords and (i)%3==0 and len(paragraph_list[i]) >3:
+        mad_lib_list.append("{}")
+    else:
+        mad_lib_list.append(paragraph_list[i])
+
+#merging the paragraph back into a single string
+madlib= ' '.join(mad_lib_list)
+
+ #outputting the madlib paragraph to a new txt file   
+with open('madlib_blank.txt', 'w') as blank:
+    blank.write(madlib)
+    
+
+
+
